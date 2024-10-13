b92306bf858761064471b3ea6548697abcb95e14
ammaraghafoor <abdulghafoor83166@gmail.com>
2024-10-13 03:04:37 -0700

    Add files via upload

---
+++ atm.ts
@@ -0,0 +1,62 @@
+
+import inquirer from "inquirer";
+
+interface anstype{
+    userid:string,
+    userpin:number,
+    accounttype:string,
+    transactiontype:string,
+    amount:number,
+
+}
+const answer :anstype =await inquirer.prompt([
+    {
+        type:"input",
+        name : "userid",
+        message: "enter id"
+
+    },
+    {
+        type:"number",
+        name : "userpin",
+        message: "enter pin",
+
+    },
+    {
+        type:"list",
+        name : "transactiontype",
+        choices:["fast cash","withdraw"],
+        message: "select transaction type",
+
+    },
+    {
+        type:"list",
+        name : "amount",
+        choices:["5000","10,000","25,000"],
+        message: "select transaction type",
+        when(answer){
+            return answer.transactiontype == "withdraw"
+        }
+
+    },
+    {
+        type:"list",
+        name : "accounttype",
+        choices:["current","saving"],
+        message: "account detail",
+
+    }
+])
+if(answer.userid && answer.userpin)
+{
+    const balance = Math.floor(Math.random()*10000);
+    console.log(balance);
+    const enterdamount =answer.amount;
+    if(balance<=enterdamount){
+        const remaining =balance -enterdamount;
+        console.log("your remaing balance is ",remaining)
+    }
+    else{
+        console.log ("insuficient balance")
+    }
+}
